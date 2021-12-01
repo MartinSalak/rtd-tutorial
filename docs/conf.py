e73402922d82b7ed17e2d33585c0b900540f55ff
Martin Salak <89633938+msalak-scia@users.noreply.github.com>
2021-12-01 11:08:32 +0100

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -3,7 +3,7 @@ import importlib
 extensions = ['myst_parser']
 source_suffix = ['.rst', '.md']
 master_doc = 'index'
-project = u'gitbookdocumentation'
+project = u'rtd-tutorial'
 
 html_css_files = [
     'custom.css',
