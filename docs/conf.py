2a51cd85fb52891ea7556c5c02b2dffe19d07e55
SCIA nv <89633938+msalak-scia@users.noreply.github.com>
2021-12-01 11:00:13 +0100

    rtd requried stuff

---
+++ docs/conf.py
@@ -0,0 +1,10 @@
+import importlib
+
+extensions = ['myst_parser']
+source_suffix = ['.rst', '.md']
+master_doc = 'index'
+project = u'gitbookdocumentation'
+
+html_css_files = [
+    'custom.css',
+]
